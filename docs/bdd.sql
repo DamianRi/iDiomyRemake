e47823202925d2b81bd8f4436c01df368977a9be
DamianRi <dams_rive@ciencias.unam.mx>
2019-11-24 21:44:38 -0600

    Actualizando sql base de datos

--- docs/bdd.sql
+++ docs/bdd.sql
@@ -7,16 +7,5 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-
-CREATE TABLE scores(
-    idscore INT NOT NULL AUTO_INCREMENT,
-    iduser  INT NOT NULL,
-    idioma VARCHAR(50) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    nivel INT NOT NULL,
-    score INT NOT NULL,
-    FOREIGN KEY (iduser) REFERENCES users(id),
-    PRIMARY KEY (idscore, iduser, idioma, categoria, nivel)
+    score INT NOT NULL;
 );
